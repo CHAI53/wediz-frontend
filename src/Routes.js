66cbd05c2aab3b9f6b82b62c0f5bf21167f8c05c
CHAI53 <skymise@gmail.com>
2019-11-15 23:27:41 +0900

    충돌 재차 해결

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./Pages/Login";
-<<<<<<< HEAD
 import ProjectView from "./Pages/ProjectView/ProjectView";
-=======
 import MainPage from "./Pages/MainPage";
 import MakerMaking from "./Pages/MakerMaking/MakerMaking";
 import EmailSignup from "Pages/EmailSignup";
->>>>>>> 9d1d9b9dfe42df70dff27a8baca16d5c85c0dd93
 
 class Routes extends React.Component {
   render() {
